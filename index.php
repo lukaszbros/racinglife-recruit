d98b1ac1c42b4829177a231d61046dde3f25fd6d
nilpotent <mgrzecza@gmail.com>
2013-03-13 17:24:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-// put your menu and website business logic here
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> 
+        <script src="jquery.content.js"></script> 
+        <title>Zadanie</title>
+        <script>
+            $(document).ready(function(){
+                $.content('Menu.php?json');
+            });
+        </script>
+    </head>
+    <body>
+        <div id="content">
+            <div id="menu"><?php require('Menu.php');?></div>
+            <div id="file"></div>
+        </div>
+    </body>
+</html>
+
